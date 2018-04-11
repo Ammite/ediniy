7af4673cb4ed6f6ef4b9820ca33f6d4775df395a
Ammite <baa21v@gmail.com>
2018-04-11 16:45:44 +0600

    initial

---
+++ main.py
@@ -0,0 +1,199 @@
+import flask
+import constants
+from flask import Flask
+from flask import request
+from flask import jsonify
+import stringify
+import constants
+import re
+import telebot
+import requests
+import json
+import sql
+
+# from flask_sslify import SSLify
+
+app = Flask(__name__)
+URL = constants.URLc
+
+if_in_contacts = False
+login = False
+passwd = False
+login_that_uses = ''
+passwd_that_uses = ''
+
+
+def write_json(data, filename='answer.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+def get_updates():
+    url = URL + 'getUpdates'
+    r = requests.get(url)
+    return r.json()
+
+
+def send_message(chat_id, text='', parse_mode='Markdown'):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text, 'parse_mode': parse_mode}
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+def send_location(chat_id, lat, lon):
+    url = URL + 'sendLocation'
+    answer = {'chat_id': chat_id, 'latitude': lat, 'longitude': lon}
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+def city(chat_id, message):
+    lib = constants.library_of_places[message]
+    send_message(chat_id, "Город: {} \nАдрес: {}\nНомера: {}, {}\nНа карте:"
+                 .format(lib['city'], lib['address'], lib['number1'], lib['number2']))
+    send_location(chat_id, lib['coor_lat'], lib['coor_lon'])
+
+
+def news(chat_id):
+    send_message(chat_id, "Канал новостей компании \"Единый Выбор\": @ediniyvibornews")
+
+
+def programs(chat_id):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': 'Выберите программу.',
+              'reply_markup': {"keyboard": [["50 нa 50"], ["Сeмья"], ["Нaзaд"]]}}
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+def programs_data(chat_id, message):
+    if message == "Сeмья":
+        send_message(chat_id,
+                     "Срок получения, месяцев: {}\nПервоначальный взнос  от стоимости квартиры, %: {}\nСрок рассрочки, лет: {}\nЦелевой взнос, %: {}".format(
+                         "6-12", "25", "до 9", "2"))
+    if message == "50 нa 50":
+        send_message(chat_id,
+                     "Срок получения, месяцев: {}\nПервоначальный взнос  от стоимости квартиры, %: {}\nСрок рассрочки, лет: {}\n".format(
+                         "3-6", "50", "до 9"))
+
+
+def work_with_text():
+    r = request.get_json()
+    global if_in_contacts
+    global login
+    global passwd
+    chat_id = r['message']['chat']['id']
+    if 'text' in r['message']:
+        message = r['message']['text']
+    else:
+        message = ''
+    print(chat_id, message)
+    if login and is_number(message):
+        take_data(chat_id, message)
+
+    elif message == '/menu' or message == "Нaзaд" or message == "/start":
+        if_in_contacts = False
+        summon_menu(chat_id)
+
+    elif message == "Контакты":
+        contacts(chat_id)
+
+    elif message == "Новости":
+        news(chat_id)
+
+    elif message == "Программы на получение недвижимости":
+        programs(chat_id)
+
+    elif message == "50 нa 50" or message == "Сeмья":
+        programs_data(chat_id, message)
+
+    elif message in constants.cities:
+        city(chat_id, message)
+
+    elif message == "Вопрос-ответ":
+        send_message(chat_id, "Ознакомить можно [тут](http://first-kz.kz/index.php#Layer17)", 'Markdown')
+
+    elif message == 'Личный кабинет':
+        open_sing_in_payers(chat_id)
+
+    elif message == 'Вoйти в кабинет очереди':  # Useful
+        pass
+
+    elif message == 'Вoйти в кабинет пайщика':  # Useful
+        open_sing_in_payers(chat_id)
+
+    elif message in constants.library_of_funcs:
+        send_message(chat_id, "[WIP]\nЕще не работает.")
+
+    else:
+        send_message(chat_id, "Не понимаю введенные данные")
+
+
+def take_data(chat_id, message):
+    sql.take_data_from_db(chat_id, message)
+
+
+def is_number(message):
+    if len(message) == 11 or len(message) == 12:
+        if message[:-10] == '+7':
+            message = '8' + login[2:]
+        if message.isdigit():
+            return True
+        else:
+            return False
+
+
+def open_sing_in_payers(chat_id):
+    global login
+    login = True
+    send_message(chat_id, 'Введите логин в формате:\n"(ваш номер)".\n'
+                          'Пример:"87071212123"')
+
+
+def open_cabinet(chat_id):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': 'Выберите программу.',
+              'reply_markup': {"keyboard": [["Вoйти в кабинет очереди"], ["Вoйти в кабинет пайщика"], ["Нaзaд"]]},
+              'one_time_keyboard': 'true'}
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+def parser(pattern, message):
+    if pattern in message:
+        return True
+    else:
+        return False
+
+
+def contacts(chat_id):
+    global if_in_contacts
+    if_in_contacts = True
+    send_message(chat_id, "Наши контакты [тут](http://first-kz.kz/#Layer16)")
+
+
+def summon_menu(chat_id):
+    # user_markup = telebot.types.ReplyKeyboardMarkup()
+    url = URL + 'sendMessage'
+    # name = "amas"
+    answer = {'chat_id': chat_id, 'text': 'Главное меню.', 'reply_markup': {"keyboard": constants.array_of_arrays}}
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        work_with_text()
+        # write_json(r)
+        # return jsonify(r)
+    return '<h1>Testing Flask!!!!</h1>'
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ sql.py
@@ -0,0 +1,70 @@
+import MySQLdb
+import main
+
+conn = MySQLdb.connect(host='sql139.main-hosting.eu',
+                       port=3306,
+                       user='u356390377_admin',
+                       passwd='rootroot1',
+                       db='u356390377_vibor')
+
+cursor = conn.cursor()
+
+conn.set_character_set('utf8')
+cursor.execute('SET NAMES utf8;')
+cursor.execute('SET CHARACTER SET utf8;')
+cursor.execute('SET character_set_connection=utf8;')
+
+
+def take_data_from_db(chat_id, message):
+    row = ['', '', '', '', '', '', '']
+    conn = MySQLdb.connect(host='sql139.main-hosting.eu',
+                           port=3306,
+                           user='u356390377_admin',
+                           passwd='rootroot1',
+                           db='u356390377_vibor')
+    cursor = conn.cursor()
+    conn.set_character_set('utf8')
+    cursor.execute('SET NAMES utf8;')
+    cursor.execute('SET CHARACTER SET utf8;')
+    cursor.execute('SET character_set_connection=utf8;')
+    main.login = False
+    login = message
+    if(login[:-10] == '+7'):
+        login = '8'+login[2:]
+    print(login)
+    cursor.execute('SELECT * FROM u356390377_vibor.usersdb WHERE Number = \'{}\''.format(login))
+    row = cursor.fetchone()
+    if row == None:
+        print("In except")
+        main.send_message(chat_id, "Неправильный логин!")
+        main.open_sing_in_payers(chat_id)
+    else:
+        print(row)
+        list_of_pays = [
+            ('50-50', row[3]),
+            ('Aktay', row[3]),
+            ('Atyray', row[3]),
+            ('Semya', row[3])
+        ]
+        for i in list_of_pays:
+            cursor.execute("SELECT * FROM `{}` WHERE Name = '{}';".format(i[0], i[1]))
+            data = cursor.fetchone()
+            if data is None:
+                continue
+            cursor.execute("SELECT COUNT(Name) FROM {}".format(i[0]))
+            count = cursor.fetchall()
+            count_of_payers = str(count[0][0])
+            send = "Здравствуйте, {}! Сейчас пайщиков в кооперативе: {}. ".format(data[0], count_of_payers)
+            #print(data)
+            if data[3] == -1:
+                send += "Вы получили недвижимость на сумму {} тенге. " \
+                        "Ваш ежемесячный паевый взнос состовляет {} тенге. "\
+                    .format(data[1], data[2])
+            else:
+                send += "Ваша запрашиваемая сумма недвижимости состовляет {} тенге. " \
+                        "Ваш паевый взнос состовляет {} тенге." \
+                        "Ваше место в очереди: {}".format(data[1], data[2], data[3])
+            main.send_message(chat_id, send)
+            break
+    conn.close()
+
